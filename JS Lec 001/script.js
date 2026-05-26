cca5d012fa499792267c666c7404129f8528ef02
Xiekhar <xiekhar@gmail.com>
2026-05-26 13:34:42 +0530

    Files rearranged

--- JS Lec 001/script.js
+++ JS Lec 001/script.js
@@ -1,64 +1,46 @@
-
-//Data Types:-
-//JS has 2 big categories: Primitive Data Types, Non-Primitive Data Types...
-
-
-//Primitive Data Types: (single/simple values):-
-
-let name = "Ginger"; //String
-let follow = true; //Boolean (true or false)
-let box; //Undefined (No value assigned)
-let fill = null; //Null (deliberately keeping it empty to assign value later)
-let big = 142635837n; //BigInt (normal numbers have a limit in JS so we use BigInt... notice 'n' in the end of the number that 'n' tells us that it's a BigInt)
-let id = Symbol(); //Symbol (Rare advanced primitive type used to create unique values)
-
-//Non-Primitive Data Types: (Bigger and more complex):-
-
-//Object
-let user = {
-    name: "ginger",
-    age: 20
-};
-console.log(user);
-
-//Array
-let colors = ["red", "green", "blue"]; 
-
-//Functions
-function greet() {
-    console.log("Hello");
-}
-greet();    //Making our own reusable functions so that we don't have to write every logic again and again. Or to say it simply-- so that we can avoid repeating the code.
+console.log('hello js')
+console.warn('This is warning')
+console.error('This is Error')
+console.table(['Robin', 'Hancock', 'Fubuki', 'Raiden', 'Arlecchino'])
 
 
-//typeof Operator  (used to check data types):-
-let a = "Zoro";
-console.log(typeof a);
+// Variables in JS
 
-let b = 20;
-console.log(typeof b);
+//Declaration:-
+let age;
 
-let c = true;
-console.log(typeof c);
+//Initialization:-
+age = 18;
+console.log(age);
 
-let d = 2.56;
-console.log(typeof d);
-
-let e = 527356n;
-console.log(typeof e);
-
-let f = {name: "Hancock", color: "purple"};
-console.log(typeof f);
-
-let g = ["Boa Hancock", 'Nico Robin', "Shirahoshi"];
-console.log(typeof g);    //This is the catch. JS treats arrays as objects so it will also return as object.
-
-function Nami() {
-    console.log("Nami");
-}
-console.log(typeof Nami);
+//Both Declaration and Initialization together :-
+let year = 2020;
+console.log(year);
 
 
-//JavaScript later aaded a special method to check arrays properly:-
-let i = ["Hancock", "Robin", "Shirahoshi"];
-console.log(Array.isArray(i));  //Answer will be in true/false since it's a boolean way of checking the particular data type.
+
+// There are three keywords of variable in JS:-
+
+//let
+let name = "Hancock";
+// variable value can be changed later...
+name = "Nico Robin";
+console.log(name);
+
+//const
+const pi = 3.14;  //Const must be declared and initialized together.
+// variable value won't change and throws error...
+pi = 24;
+console.log(pi);
+
+//var
+var phase = "first";
+// old way of declaring variables. mostly avoided in modern JS. Var behaves differently with Scope and Hoisting... 
+
+
+
+// JS is case-sensitive. 
+let name = "a";
+let Name = "b";
+let NAME = "c";
+// These all are different variables since as mentioned JS is a case-sensitive language.
